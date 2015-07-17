221da7b0618e4dfca1489d1306c562efbedb8e72
Daniel Moran <danielheth@hotmail.com>
2015-07-17 17:57:15 -0500

    adding signing step to build process

--- build.bat
+++ build.bat
@@ -2,6 +2,26 @@ set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 call %msBuildDir%\msbuild.exe  wu4tanium.sln /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 set msBuildDir=
 
+
+
+
+::ONLY SIGN IF THE CERT IS AVAILABLE
+if exist "C:\SignCert\MoranIT.pfx" (
+	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "M0r@n!T" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\bin\Release\wu4tanium.exe"
+) else (
+	echo Skipping signing since we're not on build server.
+)
+
+
 pushd wu4tanium\bin\Release\
 7za.exe a -tzip wu4tanium.zip wu4tanium.exe Interop.WUApiLib.dll
-cd ../../../
+pushd %WORKSPACE%\
+
+
+
+::ONLY SIGN IF THE CERT IS AVAILABLE
+if exist "C:\SignCert\MoranIT.pfx" (
+	signtool.exe sign /f "C:\SignCert\MoranIT.pfx" /p "M0r@n!T" /t "http://timestamp.verisign.com/scripts/timestamp.dll" "%WORKSPACE%\bin\Release\wu4tanium.zip"
+) else (
+	echo Skipping signing since we're not on build server.
+)
